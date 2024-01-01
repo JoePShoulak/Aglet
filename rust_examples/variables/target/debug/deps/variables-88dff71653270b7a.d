5702d195229c3a39c556d24ea484cee63c249e8a
Ethan Bettenga <ebettenga@gmail.com>
2024-01-01 15:28:57 -0600

    examples

---
+++ rust_examples/variables/target/debug/deps/variables-88dff71653270b7a.d
@@ -0,0 +1,5 @@
+/Users/ethanbett/Documents/Repos/Aglet/rust_examples/variables/target/debug/deps/libvariables-88dff71653270b7a.rmeta: src/main.rs
+
+/Users/ethanbett/Documents/Repos/Aglet/rust_examples/variables/target/debug/deps/variables-88dff71653270b7a.d: src/main.rs
+
+src/main.rs:
